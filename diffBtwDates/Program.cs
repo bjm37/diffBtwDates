2e5da01f231f1bcea10ee1350cecc20a3c40b72e
bjm37 <bjm37@calvin.edu>
2017-08-19 22:20:09 -0400

    ask user for the date

--- diffBtwDates/Program.cs
+++ diffBtwDates/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace diffBtwDates
 {
     class Program
     {
         static void Main(string[] args)
+        /*Write a C# console application to calculate the duration between two dates in Years, Months and Days. 
+         The user should enter two dates as input, then the program will show the result of the difference in time between these two dates. 
+         Show the difference in days, hours, and minutes*/
         {
+            // Define two dates.
+            DateTime date1 = new DateTime(1990, 11, 6, 9, 30, 45);
+            DateTime date2 = new DateTime(1993, 8, 31, 6, 0, 8);
+            
+            // Calculate the interval between the two dates in Years, months, and days
+            TimeSpan interval = date2 - date1;
+            Console.WriteLine(interval.ToString());
         }
     }
 }
